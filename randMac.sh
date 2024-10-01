ccbde256cbe117b2f4c5dfd7abab3376252fd5a2
timothytrue <timothytrue@protonmail.com>
2024-09-30 18:37:45 -0600

    rand mac

---
+++ randMac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Prompt the user for the network interface
+read -p "Enter the network interface you want to randomize (e.g., eth0, wlan0, etc.): " INTERFACE
+
+while true; do
+    # Disable the network interface
+    echo "Disabling $INTERFACE..."
+    sudo ip link set $INTERFACE down
+
+    # Randomize the MAC address
+    echo "Randomizing MAC address for $INTERFACE..."
+    sudo macchanger -r $INTERFACE
+
+    # Re-enable the network interface
+    echo "Re-enabling $INTERFACE..."
+    sudo ip link set $INTERFACE up
+
+    # Wait for 30 minutes before repeating the process
+    echo "Waiting for 30 minutes..."
+    sleep 1800
+done
+
